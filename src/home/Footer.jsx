e711611c5eeae0ab139a341e08a60dc72f6f1afc
bHabawel <rosshabawel12@gmail.com>
2024-10-03 21:14:29 +0800

    message

---
+++ src/home/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="py-[64px]" data-aos="fade-up" data-aos-duration="1000">
+      <p className="text-center font-semibold text-white">
+        © {new Date().getFullYear()} Zaidee Tayaban. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
